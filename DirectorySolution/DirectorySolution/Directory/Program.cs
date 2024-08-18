15c5f96f1092aad27d2095d0cf8707f48210568b
Mahbubur Rahman <90133937+mahbubtipu1234@users.noreply.github.com>
2024-08-18 18:30:24 +0600

    Add files via upload

---
+++ DirectorySolution/DirectorySolution/Directory/Program.cs
@@ -0,0 +1,36 @@
+using Directory.Components;
+using MudBlazor.Services;
+
+namespace Directory
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add MudBlazor services
+            builder.Services.AddMudServices();
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
